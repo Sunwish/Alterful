1f30a0355f9dc1d6de16e65564f154dbb78623dd
Sunwish <770168287@qq.com>
2021-04-13 14:03:03 +0800

    Upgrade version number to 2.1.2.101 for testing.

--- Alterful/Properties/Settings.Designer.cs
+++ Alterful/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Alterful.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace Alterful.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2.0.2.101")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2.1.2.101")]
         public string localVersion {
             get {
                 return ((string)(this["localVersion"]));
